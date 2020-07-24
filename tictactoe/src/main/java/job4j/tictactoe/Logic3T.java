5df7e3f9773ba38ee37408cceeedd712572dbeeb
Sergey Sapunov <youcanwriteme@yandex.ru>
2020-07-24 16:45:14 +0700

    Крестики-нолики на JavaFX [#235657]

--- tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
@@ -1,5 +1,8 @@
 package job4j.tictactoe;
 
+import java.util.Arrays;
+import java.util.function.Predicate;
+
 public class Logic3T {
     private final Figure3T[][] table;
 
@@ -7,15 +10,58 @@ public class Logic3T {
         this.table = table;
     }
 
+    private boolean testTable(Predicate<Figure3T> predicate) {
+        int [] rows = new int [table.length];
+        int [] cols = new int [table[0].length];
+
+        boolean mainDiagonal = true;
+        boolean secDiagonal = true;
+        boolean line = false;
+
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                if (predicate.test(table[i][j])) {
+                    cols[j]++;
+                    rows[i]++;
+                } else {
+                    if (i == j) {
+                        mainDiagonal = false;
+                    }
+                    if (i == table[0].length - j - 1){
+                        secDiagonal = false;
+                    }
+                }
+            }
+        }
+
+        for (int row: rows) {
+            if (row == 3) {
+                line = true;
+                break;
+            }
+        }
+
+        for (int col: cols) {
+            if (col == 3) {
+                line = true;
+                break;
+            }
+        }
+
+        return mainDiagonal || secDiagonal || line;
+    }
+
     public boolean isWinnerX() {
-        return false;
+        return testTable(Figure3T::hasMarkX);
     }
 
     public boolean isWinnerO() {
-        return false;
+        return testTable(Figure3T::hasMarkO);
     }
 
     public boolean hasGap() {
-        return true;
+        return Arrays.stream(table)
+                .flatMap(Arrays::stream)
+                .anyMatch(figure3T -> !(figure3T.hasMarkO() || figure3T.hasMarkX()));
     }
 }
